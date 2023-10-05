fadb1113a6b03c6d44e31287c5d8e94f27e1e430
Ayush Tripathi <arnavtripathi1234@gmail.com>
2023-10-05 18:26:43 +0530

    update

--- Day 3/nested loop.cpp
+++ Day 3/nested loop.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 #define endl '\n'
 
-
 /*
-print pattern :  
+print pattern :
+
+n = 4
+
 1 2 3 4
 5 6 7 8
 9 10 11 12
@@ -17,9 +19,11 @@ int main()
   cin >> rows >> cols;
 
   int value = 1;
-  for (int i = 1; i <= rows; i++) { // current row number
+  for (int i = 1; i <= rows; i++)
+  { // current row number
 
-    for (int j = 1; j <= cols; j++) { // current col number
+    for (int j = 1; j <= cols; j++)
+    { // current col number
       cout << value << ' ';
 
       value++;
